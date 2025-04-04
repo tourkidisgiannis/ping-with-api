482dbc009c36329a56726c991510493b5287a703
tourkidisgiannis <125009763+tourkidisgiannis@users.noreply.github.com>
2025-04-04 17:20:37 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>Next Ts Starter</h1>
+      </div>
+    </section>
+  )
+}
+
+export default HomePage
